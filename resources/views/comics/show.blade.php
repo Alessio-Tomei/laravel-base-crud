f7c743380f400678f5220c3e88adcec898387a4e
Alessio-Tomei <alessiotomei22@gmail.com>
2022-03-09 18:05:23 +0100

    first commit

---
+++ resources/views/comics/show.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.base')
+
+@section('title','comic')
+
+@section('content')
+    <img src="{{$comic->thumb}}" alt="">
+    <h1>{{$comic->title}}</h1>
+    <h2>{{$comic->series}}</h2>
+    <p>{{$comic->type}}</p>
+    <p>{{$comic->description}}</p>
+    <a href="{{route("comics.index")}}"><button type="button" class="btn btn-primary">back</button></a>
+@endsection
